8c0d709242a06ea3840b526e3f759ff1dd457bd7
Noah McCaskill <noah.mccaskill@mths.ca>
2022-03-22 14:36:46 +0000

    added area and circumference

---
+++ area_and_circumference.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by: Noah McCaskill
+# Created on: March 2022
+# This program calculates the area and perimeter of a rectangle
+#    with dimensions 5cm x 3cm
+
+import math 
+
+def main():
+    # this function calculates the area and perimeter
+    print("If a circle has a radius of 15 mm: ")
+    print("")
+    print("Area is {} mm².".format(math.pi*15**2))
+    print("Circumference is {} mm.".format(2 *math.pi*15))
+    
+
+if __name__ == "__main__":
+    main()
+    
